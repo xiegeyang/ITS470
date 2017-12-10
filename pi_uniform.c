9be99dbdfdb6d4dc45003d0049773f7d2358752e
Geyang Xie <xiegeyang@gmail.com>
2017-12-10 10:49:57 -0600

    ds

--- pi_uniform.c
+++ pi_uniform.c
@@ -83,9 +83,10 @@ int main(int argc,char *argv[], int target) {
                         }
             total += sum;
             MPI_Reduce(&total, &realTotal, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-            if (myrank == 0)
-                printf("sum is %f", realTotal);
+
           }
+          if (myrank == 0)
+              printf("sum is %f", realTotal);
         }
     }
     MPI_Finalize();
