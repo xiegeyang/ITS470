fc6b18cb8d86d60933f5c9a20edd241f38cbaf29
Geyang Xie <xiegeyang@gmail.com>
2017-12-10 10:23:49 -0600

    ds

--- pi_uniform.c
+++ pi_uniform.c
@@ -49,7 +49,7 @@ int main(int argc,char *argv[], int target) {
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&nproc);
     MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
-    n = 1000;
+    n = 0;
     while (!done)
     {
         MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
